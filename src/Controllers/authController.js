828877be959f762d46018dd3afd3d10cffed86bf
vinicius <vinicius.coassis@gmail.com>
2022-09-09 12:38:03 -0300

    refactor: divide app.js entities into diferent documents pt 2 - authControllers

---
+++ src/Controllers/authController.js
@@ -0,0 +1,74 @@
+import { db } from "../db.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import joi from "joi";
+
+const userSchema = joi.object({
+  email: joi.required(),
+  password: joi.required(),
+});
+
+const newUserSchema = joi.object({
+  name: joi.string().required(),
+  email: joi
+    .string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+});
+
+async function signUp(req, res) {
+  const newUser = req.body;
+
+  const { error, value } = newUserSchema.validate(newUser, {
+    abortEarly: false,
+  });
+  if (error) {
+    res.status(400).send(error.details.map((value) => value.message));
+  }
+
+  try {
+    await db.collection("users").insertOne({
+      ...newUser,
+      password: bcrypt.hashSync(newUser.password, 10),
+    });
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function signIn(req, res) {
+  const { email, password } = req.body;
+
+  const { error, value } = userSchema.validate(
+    { email, password },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).send(error.details.map((value) => value.message));
+  }
+  try {
+    const user = await db.collection("users").findOne({ email });
+
+    let equalPassoword = bcrypt.compareSync(password, user.password);
+    if (user && equalPassoword) {
+      const token = uuid();
+
+      await db.collection("sessions").insertOne({ token, userId: user._id });
+
+      res.status(201).send(token);
+    } else {
+      return res.status(401).send("email ou senha inválidos");
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+export { signUp, signIn };
--- src/app.js
+++ src/app.js
@@ -1,83 +1,16 @@
 import express from "express";
 import cors from "cors";
-import joi from "joi";
 import { getRegisters, RegisterNew } from "./Controllers/registerController.js";
-import { db } from "./db.js";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
+import { signIn, signUp } from "./Controllers/authController.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const newUserSchema = joi.object({
-  name: joi.string().required(),
-  email: joi
-    .string()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    })
-    .required(),
-  password: joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
-});
+app.post("/sign-up", signUp);
 
-const userSchema = joi.object({
-  email: joi.required(),
-  password: joi.required(),
-});
-
-app.post("/sign-up", async (req, res) => {
-  const newUser = req.body;
-
-  const { error, value } = newUserSchema.validate(newUser, {
-    abortEarly: false,
-  });
-  if (error) {
-    res.status(400).send(error.details.map((value) => value.message));
-  }
-
-  try {
-    await db.collection("users").insertOne({
-      ...newUser,
-      password: bcrypt.hashSync(newUser.password, 10),
-    });
-
-    res.sendStatus(201);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.post("/sign-in", async (req, res) => {
-  const { email, password } = req.body;
-
-  const { error, value } = userSchema.validate(
-    { email, password },
-    { abortEarly: false }
-  );
-
-  if (error) {
-    res.status(422).send(error.details.map((value) => value.message));
-  }
-  try {
-    const user = await db.collection("users").findOne({ email });
-
-    let equalPassoword = bcrypt.compareSync(password, user.password);
-    if (user && equalPassoword) {
-      const token = uuid();
-
-      await db.collection("sessions").insertOne({ token, userId: user._id });
-
-      res.status(201).send(token);
-    } else {
-      return res.status(401).send("email ou senha inválidos");
-    }
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+app.post("/sign-in", signIn);
 
 app.get("/registers", getRegisters);
 
